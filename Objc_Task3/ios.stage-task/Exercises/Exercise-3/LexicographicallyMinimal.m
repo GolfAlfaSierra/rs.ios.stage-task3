739ee795f6b14b5ec6f1123cafb56e30b4de3682
Artyom Sog <tomas.tomson@Mail.ru>
2021-05-23 16:44:24 +0300

    feat: exercise three done

--- Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
@@ -9,7 +9,34 @@
 @implementation LexicographicallyMinimal
 
 -(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2 {
-    return nil;
+    NSMutableString *mutableString1 = [NSMutableString stringWithString:string1];
+    NSMutableString *mutableString2 = [NSMutableString stringWithString:string2];
+    NSMutableString *result = [[NSMutableString alloc] init];
+    
+    NSRange first = NSMakeRange(0, 1);
+
+    
+    while ((mutableString1.length != 0) && (mutableString2.length != 0)) {
+        
+        char ch1 = [mutableString1 characterAtIndex:0];
+        char ch2 = [mutableString2 characterAtIndex:0];
+                
+        if (ch1 <= ch2) {
+            [result appendFormat:@"%c", ch1];
+            [mutableString1 deleteCharactersInRange:first];
+        } else {
+            [result appendFormat:@"%c", ch2];
+            [mutableString2 deleteCharactersInRange:first];
+        }
+    }
+    
+    if (mutableString1.length == 0) {
+        [result appendString:mutableString2];
+    } else {
+        [result appendString:mutableString1];
+    }
+    
+    return result;
 }
 
 @end
