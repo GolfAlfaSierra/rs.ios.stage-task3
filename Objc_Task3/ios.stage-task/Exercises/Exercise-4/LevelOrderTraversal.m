e9972a1688f4407d2d601c4a48b750c8ada87d72
Artyom Sog <tomas.tomson@Mail.ru>
2021-05-24 17:24:47 +0300

    feat: exercise four done

--- Objc_Task3/ios.stage-task/Exercises/Exercise-4/LevelOrderTraversal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-4/LevelOrderTraversal.m
@@ -1,5 +1,44 @@
 #import "LevelOrderTraversal.h"
 
+NSUInteger LevelOrderTraversalForTreeNodes(NSArray *numbersTree, NSMutableArray<NSMutableArray *> *levelOrderedData, NSUInteger level, NSUInteger startIndex) {
+    
+    
+    
+    typedef enum {
+        LeftNode,
+        RightNode
+    } NodeType;
+    
+    NSUInteger ptrIndex = startIndex;
+    
+    // начать цикл, брать левый элемент. 
+    
+    for (NodeType nodeType = LeftNode; nodeType <= RightNode && ptrIndex < numbersTree.count;) {
+        
+        NSObject *node = numbersTree[ptrIndex++];
+        
+        if ([node isKindOfClass:NSNumber.class]) {
+            
+            if (levelOrderedData.count <= level) {
+                [levelOrderedData addObject:[NSMutableArray new]];
+            }
+            
+            
+            [levelOrderedData[level] addObject:node];
+            
+            ptrIndex = LevelOrderTraversalForTreeNodes(numbersTree, levelOrderedData, level+1, ptrIndex);
+        }
+        
+        ++nodeType;
+    }
+    return ptrIndex;
+}
+
 NSArray *LevelOrderTraversalForTree(NSArray *tree) {
-    return nil;
+    NSMutableArray<NSMutableArray *> *resultArray = [NSMutableArray array];
+    NSUInteger level = 0;
+    
+    LevelOrderTraversalForTreeNodes(tree, resultArray, level, 0);
+    
+    return [resultArray copy];
 }
